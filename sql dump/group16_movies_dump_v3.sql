5d5b72c33c2a38abb9eb43f0b690c35bd45578b1
James Neale <jamesneale2004@gmail.com>
2024-05-15 12:23:35 +0200

    added auto inc to titles

--- sql dump/group16_movies_dump_v3.sql
+++ sql dump/group16_movies_dump_v3.sql
@@ -11679,7 +11679,7 @@ DROP TABLE IF EXISTS `titles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `titles` (
-  `title_id` int(11) NOT NULL,
+  `title_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `release_year` varchar(4) NOT NULL,
   `description` varchar(255) NOT NULL,
