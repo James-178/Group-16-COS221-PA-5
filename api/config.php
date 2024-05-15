8ab50db76b02bc1a9efd89470cb0027736d2b243
eugenvosloo <128828064+eugenvosloo@users.noreply.github.com>
2024-05-16 00:33:36 +0200

    Add files via upload

---
+++ api/config.php
@@ -0,0 +1,7 @@
+<?php
+// Database configuration
+define('DB_SERVER', 'wheatley.cs.up.ac.za');
+define('DB_USER', 'u20445696');                              //studentnumber
+define('DB_PASSWORD', '');  //phpmyadmin password
+define('DB_NAME', '');                 //database name
+?>
